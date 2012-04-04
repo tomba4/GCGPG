3b38bf885aed80a2c2bd6fdca5f772ee2c8be77b
tomba4 <tomba4@gmail.com>
2012-04-04 23:04:11 +0200

    1st exercise done

---
+++ sdl_01/include/CApp.h
@@ -0,0 +1,22 @@
+#ifndef CAPP_H
+#define CAPP_H
+
+#include <SDL/SDL.h>
+
+class CApp
+{
+    public:
+        CApp();
+        int OnExecute();
+        bool OnInit();
+        void OnEvent(SDL_Event* Event);
+        void OnLoop();
+        void OnRender();
+        void OnCleanup();
+    protected:
+    private:
+        bool Running;
+        SDL_Surface* Surf_Display;
+};
+
+#endif // CAPP_H
---
+++ sdl_01/src/CApp.cpp
@@ -0,0 +1,77 @@
+#include "CApp.h"
+
+CApp::CApp()
+{
+    Running = true;
+    Surf_Display = NULL;
+}
+
+int CApp::OnExecute()
+{
+    if(OnInit() == false)
+    {
+        return -1;
+    }
+
+    SDL_Event Event;
+
+    while(Running)
+    {
+        while(SDL_PollEvent(&Event))
+        {
+            OnEvent(&Event);
+        }
+
+        OnLoop();
+        OnRender();
+    }
+
+    OnCleanup();
+
+    return 0;
+}
+
+bool CApp::OnInit()
+{
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        return false;
+    }
+
+    if((Surf_Display = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void CApp::OnEvent(SDL_Event* Event)
+{
+    if(Event->type == SDL_QUIT)
+    {
+        Running = false;
+    }
+}
+
+void CApp::OnLoop()
+{
+
+}
+
+void CApp::OnCleanup()
+{
+    SDL_Quit();
+}
+
+void CApp::OnRender()
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    CApp theApp;
+
+    return theApp.OnExecute();
+}
